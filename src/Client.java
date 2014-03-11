d535a739f7eb63a1aabd4f96c8e0046601476b66
Julien <julien@guerinet.com>
2014-03-11 10:45:00 -0400

    made receiveMessage blocking

--- src/Client.java
+++ src/Client.java
@@ -12,6 +12,7 @@ public class Client {
     private static final int SERVER_PORT = 5000;
 
     private static Socket clientSocket;
+    private static ReceiveThread receiveThread;
 
     private static boolean hasAccount = false;
     private static boolean loggedOn = false;
@@ -27,6 +28,10 @@ public class Client {
 
         //Set up the CLI reader
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in,"UTF-8"));
+
+        //Set up the receiving thread
+        receiveThread = new ReceiveThread();
+        receiveThread.run();
 
 
 
@@ -118,34 +123,74 @@ public class Client {
         out.flush();
     }
 
-    public static  Message receiveMessage() throws IOException{
-        InputStream in = new BufferedInputStream(clientSocket.getInputStream());
-        int bytesAvailable = in.available();
+    public static class ReceiveThread extends Thread{
+        private Message receivedMessage;
+        private boolean run;
 
-        //Wait until we have at least 12 bytes available so we can find out how much data we have to read
-        while(bytesAvailable < 12){
-            bytesAvailable = in.available();
+        public ReceiveThread(){
+            run = true;
         }
 
-        //Retrieve the first 12 bytes, convert them into numbers
-        byte[] messageTypeBytes = new byte[4];
-        in.read(messageTypeBytes);
-        int messageType = ByteBuffer.wrap(messageTypeBytes).getInt();
+        @Override
+        public void run(){
+            try {
+                while(run){
+                    //Get the message
+                    receivedMessage = receiveMessage();
 
-        byte[] subMessageTypeBytes = new byte[4];
-        in.read(subMessageTypeBytes);
-        int subMessageType = ByteBuffer.wrap(subMessageTypeBytes).getInt();
+                    //Check if it's a message that the main thread would need
+                    //Main thread needs all message except the inactivity logout and the querying of the messages
+                    //TODO Add the inactivity logout here
+                    if(receivedMessage.getMessageType() == MessageType.QUERY_MESSAGES){
+                        //Keep on running the code if ever it's just a query
+                        run = true;
+                    }
+                    else{
+                        run = false;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-        byte[] dataSizeBytes = new byte[4];
-        in.read(dataSizeBytes);
-        int dataSize = ByteBuffer.wrap(dataSizeBytes).getInt();
+        public Message getReceivedMessage(){
+            //Continue running the code
+            run = true;
+            while(receivedMessage == null){}
 
-        //Retrieve the correct number of bytes for the data, convert it into a String
-        byte[] dataBytes = new byte[dataSize];
-        in.read(dataBytes);
-        String data = new String(dataBytes);
+            return this.receivedMessage;
+        }
 
-        //Return a message with all of this data
-        return new Message(MessageType.getMessageType(messageType), subMessageType, data);
+        private Message receiveMessage() throws IOException{
+            InputStream in = new BufferedInputStream(clientSocket.getInputStream());
+            int bytesAvailable = in.available();
+
+            //Wait until we have at least 12 bytes available so we can find out how much data we have to read
+            while(bytesAvailable < 12){
+                bytesAvailable = in.available();
+            }
+
+            //Retrieve the first 12 bytes, convert them into numbers
+            byte[] messageTypeBytes = new byte[4];
+            in.read(messageTypeBytes);
+            int messageType = ByteBuffer.wrap(messageTypeBytes).getInt();
+
+            byte[] subMessageTypeBytes = new byte[4];
+            in.read(subMessageTypeBytes);
+            int subMessageType = ByteBuffer.wrap(subMessageTypeBytes).getInt();
+
+            byte[] dataSizeBytes = new byte[4];
+            in.read(dataSizeBytes);
+            int dataSize = ByteBuffer.wrap(dataSizeBytes).getInt();
+
+            //Retrieve the correct number of bytes for the data, convert it into a String
+            byte[] dataBytes = new byte[dataSize];
+            in.read(dataBytes);
+            String data = new String(dataBytes);
+
+            //Return a message with all of this data
+            return new Message(MessageType.getMessageType(messageType), subMessageType, data);
+        }
     }
 }
